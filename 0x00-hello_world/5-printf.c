c3f5b4b3cce9facf935bfe69689db7aca14b966d
Seid Ibrahim <seidibrahimd@gmail.com>
2023-02-16 10:16:54 -0800

    cool

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *main - printf
+ *Return: zero
+ */
+int main(void)
+{
+	printf("with proper grammar',' but the outcome is a piece of art','");
+	return (0);
+}
