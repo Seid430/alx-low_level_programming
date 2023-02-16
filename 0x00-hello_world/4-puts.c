5a7a2a896b2692f54bac6465d2d0628bcac1bca0
Seid Ibrahim <seidibrahimd@gmail.com>
2023-02-16 10:01:28 -0800

    cool

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#includes<stdio.h>
+#include<stdio.h>
 /**
  * main - aslk
  * Return: zero
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n";
+puts("\"Programming is like building a multilingual puzzle\n");
 		return (0);
 }
