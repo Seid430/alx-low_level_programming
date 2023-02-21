22912f0d479894db4365e09c8349051d9af7eef2
seid <seidibrahimd@gmail.com>
2023-02-21 19:02:23 +0000

    2nd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+ * print_alphabet - print small alphabet
+ * Return: return value can be anything
+ */
+void print_alphabet(void)
+{
+char i = 'a';
+while (i <= 'z')
+{
+	_putchar (i);
+i++;
+}
+
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include"main.h"
+/*
+ *print_alphabet_x10 - 10 times alphabet in small case
+ *Return: nothing men
+ */
+void print_alphabet_x10(void)
+{
+char i, j = 1;
+
+while (j <= 10)
+{
+	i = 'a';
+	while (i <= 'z')
+	{
+		_putchar (i);
+		i++;
+
+	}
+	_putchar('\n');
+		j++;
+
+
+
+}
+
+
+
+}
