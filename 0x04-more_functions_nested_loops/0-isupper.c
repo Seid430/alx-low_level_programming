003a653a3634f0e123307ec52cf91ee7ccb851eb
seid <seidibrahimd@gmail.com>
2023-02-23 09:38:50 +0000

    1st and 2nd

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isupper - capital letter
+ * @c: - something else
+ *Return: return 0
+ */
+int _isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ *_isdigit - digit or not
+ *c: - any integer
+ */
+int _isdigit(int c)
+{
+	if(c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+
+
+
+
+
+}
