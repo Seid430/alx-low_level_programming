c9c033095b244dc40eb1417bea68bc23f575f3cc
seid <seidibrahimd@gmail.com>
2023-02-27 19:53:12 +0000

    bonus

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -15,16 +15,18 @@ int _atoi(char *s)
 	sign = 1;
 	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 	{
-	if (*temp == '-')
-		sign *= -1;
-		temp++;
+		if (*temp == '-')
+		{
+			sign *= -1;
+			temp++;
+		}
 	}
 	if (*temp != '\0')
 	{
-	do {
-		num = num * 10 + (*temp - '0');
-		temp++;
-	} while (*temp >= '0' && *temp <= '9');
+		do {
+			num = num * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
 	}
 	return (num * sign);
 }
