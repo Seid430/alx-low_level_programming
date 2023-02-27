7c3917bfc327749bacfff1f0ba35a8f6a16039c7
seid <seidibrahimd@gmail.com>
2023-02-27 16:02:22 +0000

    string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - name of this particular function
+ * @s: name of this variable
+ * Return: none
+ */
+void print_rev(char *s)
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+	;
+for (i = i - 1; s[i] != 0; i--)
+{
+_putchar(s[i]);
+}
+}
