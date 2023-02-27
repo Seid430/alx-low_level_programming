1cf2e483cee815bf8ed44f3e9598ebde420c5f39
seid <seidibrahimd@gmail.com>
2023-02-27 17:43:45 +0000

    puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half - function name
+ * @str: string name
+ */
+void puts_half(char *str)
+{
+	int j, n, k;
+	
+	n = 0;
+	while (*(str + n) != '\0')
+	{
+		n++;
+	}
+	if (n % 2 == 0)
+	{
+		j = n / 2;
+		for (k = j; k <= (n - 1); k++)
+		{
+			_putchar(*(str + k));
+		}
+	}
+	else
+	{
+		j = (n - 1) / 2;
+		for (k = (j + 1); k <= (n - 1); k++)
+		{
+			_putchar(*(str + k));
+		}
+	}
+	_putchar('\n');
+}
