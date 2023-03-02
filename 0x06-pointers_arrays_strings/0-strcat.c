f0a13c507189ae8643aaa285c615ea59c49fa7ed
seid <seidibrahimd@gmail.com>
2023-03-02 07:04:54 +0000

    my own strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,40 +5,19 @@
  * @dest: -lkjga
  * @src: source
  */
-
 char *_strcat(char *dest, char *src)
 {
-int length, j;
+int i, j;
 
 length = 0;
-while (dest[length] != '\0')
+while (dest[i] != '\0')
 {
-length++;
+i++;
 }
-for (j = 0; src[j] != '\0'; j++, length++)
+for (j = 0; src[j] != '\0'; j++, i++)
 {
-dest[length] = src[j];
+dest[i] = src[j];
 }
-dest[length] = '\0';
+dest[i] = '\0';
 return (dest);
 }
-/*
-char *_strcat(char *dest, char *src)
-{
-	char i, j;
-
-	i = 0;
-	j = 0;
-	while(c[i] != '\0')
-	{
-		i++;
-	}
-	while(d[j] != '\0')
-	{
-		c[i] = d[j];
-		i++;
-		j++;
-	}
-	c[i] = '\0';
-	return (c);
-}*/
