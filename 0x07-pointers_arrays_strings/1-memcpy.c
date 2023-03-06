d70f82edcf870557895db8296ec25060420e7d30
seid <seidibrahimd@gmail.com>
2023-03-06 12:42:10 +0000

    aklg;al

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  *_memcpy - name of this function
- *dest: destination of copied file
- *src: source of the file to be copied
- *n: number of memory bytes
+ *@dest: destination of copied file
+ *@src: source of the file to be copied
+ *@n: number of memory bytes
  *Return: dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
