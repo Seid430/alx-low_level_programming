b9c65a01f97fb8d4b493a3dabd984e0af86fad5c
Seid430 <seidibrahimd@gmail.com>
2023-03-12 19:57:54 +0000

    seid|

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * _puts - writes a function that prints a string to the standard output
- * @str: string
- * Return: 0
+ *_puts - prints a string
+ * @str: string to print
+ *
+ * Description: prints a string
+ * On success: return the number of characters printed
  */
+
 void _puts(char *str)
 {
-for (; *str != '\0'; str++)
-{
-	_putchar(*str);
-}
+while (*str)
+_putchar(*str++);
+
 _putchar('\n');
 }
