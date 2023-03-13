8be1401c65868a102f6411ac94d4f2584a858587
Seid430 <seidibrahimd@gmail.com>
2023-03-13 04:21:03 +0000

    seid

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints its name
+ * @argc: no of arg
+ * @argv: array of arg
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+
+{
+
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
