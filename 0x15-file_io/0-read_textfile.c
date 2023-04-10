ca3397e9dcc4a10476cecc956b251a24f7e4c971
Seid430 <seidibrahimd@gmail.com>
2023-04-10 18:34:56 +0000

    final

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,37 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output
- *
- * @filename: name of text file
- * @letters: number of letters it should read and print.
- *
- * Return: the actual number of letters it could read and print
- * if the file can not be opened or read, return 0
- * if filename is NULL return 0
- * if write fails or does not write the expected amount of bytes, return 0
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t da, wr, re;
-	char *buff;
-
-	if (filename == NULL)
+	int fd;
+	int i, y;
+	char *buf;
+	if (!filename)
 		return (0);
-
-	buff = malloc(letters);
-	if (buff == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 		return (0);
-
-	da = open(filename, O_RDWR);
-
-	if (da == -1)
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
 		return (0);
-
-	re = read(da, buff, letters);
-	if (re == -1)
+	i = read(fd, buf, letters);
+	if (i < 0)
+	{
+		free(buf);
 		return (0);
-
-	wr = write(STDOUT_FILENO, buff, re);
-	if (wr == -1 || wr != re)
+	}
+	buf[i] = '\0';
+	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
+	{
+		free(buf);
 		return (0);
-
-	re = close(da);
-	if (re == -1)
-		return (0);
-
-	free(buff);
-	return (wr);
+	}
+	free(buf);
+	return (y);
 }
